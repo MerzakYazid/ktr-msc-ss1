424b87956894299538c8229f93a9607d7825a946
MerzakYazid <74783026+MerzakYazid@users.noreply.github.com>
2020-12-03 22:44:12 +0100

    Add files via upload

---
+++ Step1/Character/Charactere.h
@@ -0,0 +1,39 @@
+#pragma once
+#include<string.h>
+#include<iostream>
+using namespace std;
+
+class Character: public Movable
+{
+private:
+	string name;
+	const string RPGClass;
+	int life, agility, strengh, wit;
+
+public:
+	void NameSet(string);
+	string NameGet();
+
+	void RPGClassSet(string);
+	string RPGClassGet();
+
+	void LifeSet(int);
+	int LifeGet();
+
+	void AgilitySet(int);
+	int AgilityGet();
+
+	void StrenghSet(int);
+	int StrenghGet();
+
+	void WitSet(int);
+	int WitGet();
+
+	Character();
+
+	Character(string N, string R);
+
+	virtual void attack(string);
+
+	void showstats();
+}
---
+++ Step1/ktr-msc-ss1.cpp
@@ -0,0 +1,13 @@
+// ktr-msc-ss1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include "Character.h"
+
+int main()
+{
+
+	Character player3("Alex", "Character");
+        player3.showstats();
+	player3.attack("");
+}
